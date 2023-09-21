cde1d6c1c35270dd6717b28a69528b3cf088c14d
Rickard Hole Falck <rickard.hole.falck@solidify.dev>
2023-09-21 13:48:35 +0200

    Added localizer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,11 @@
+import Header from '@/components/Header'
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import { LocalizerWrapper } from 'nextjs-static-page-localizer'
 
 const inter = Inter({ subsets: ['latin'] })
+const config = require("@/messages/config")
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -16,7 +19,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <LocalizerWrapper configuration={config}>
+          <Header />
+          {children}
+        </LocalizerWrapper>
+      </body>
     </html>
   )
 }
